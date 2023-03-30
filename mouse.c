132245d71aae9ac9bc4369e9b88b3c27e7cf4733
Lars Brinkhoff <lars@nocrew.org>
2023-03-30 21:10:48 +0200

    stuff

--- mouse.c
+++ mouse.c
@@ -19,26 +19,26 @@ void write_b_mouse(u32 a, u8 x) {
 DEFAULT_READ_W(mouse)
 SAME_WRITE_W(mouse)
 
-void mouse_motion (int x, int y) {
+void mouse_motion(int x, int y) {
   mx += x;
   my -= y;
 }
 
-static void button (u8 mask, int pressed) {
+static void button(u8 mask, int pressed) {
   if (pressed)
     status_clear(mask);
   else
     status_set(mask);
 }
 
-void mouse_button_left (int pressed) {
+void mouse_button_left(int pressed) {
   button(0x20, pressed);
 }
 
-void mouse_button_middle (int pressed) {
+void mouse_button_middle(int pressed) {
   button(0x40, pressed);
 }
 
-void mouse_button_right (int pressed) {
+void mouse_button_right(int pressed) {
   button(0x80, pressed);
 }
